c55bd3e09ec5d780a680b07e7558fb1b3caeeac7
zxt <2164313511@qq.com>
2026-04-15 22:05:58 +0800

    优化了提交

--- console-vue/src/views/home/HomeIndex.vue
+++ console-vue/src/views/home/HomeIndex.vue
@@ -5,33 +5,7 @@
         <div class="header">
           <div @click="toMySpace" class="logo">拿个offer-SaaS短链接@马丁</div>
           <div style="display: flex; align-items: center">
-            <a
-              class="link-span"
-              style="text-decoration: none"
-              target="_blank"
-              href="https://nageoffer.com/shortlink/"
-              >官方文档</a
-            >
-            <a
-              class="link-span"
-              style="text-decoration: none"
-              target="_blank"
-              href="https://nageoffer.com/planet/group/"
-              >加沟通群</a
-            >
-            <a
-                class="link-span"
-                style="text-decoration: none"
-                target="_blank"
-                href="https://nageoffer.com/shortlink/video/"
-            >🔥视频教程</a
-            >
-            <a
-                class="link-span"
-                style="text-decoration: none"
-                target="_blank"
-                href="http://shortlink.nageoffer.com"
-            >演示环境</a
+
             >
             <el-dropdown>
               <div class="block">
